734a99165ec9f59385d23395bde3ff9e0129a37e
K Narayan Reddy <narayanreddy355@gmail.com>
2025-10-11 02:01:29 +0530

    changes done

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-// export const BASE_URL = "https://movie4u-backend-v6iv.onrender.com/"
-export const BASE_URL = "http://localhost:8080/"
+export const BASE_URL = "https://movie4u-backend-v6iv.onrender.com/"
